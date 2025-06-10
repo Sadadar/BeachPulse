5be80b510e1fbebe92af57687b8e19fa2847e52a
Matt Greenberg <mgreenberg@gmail.com>
2025-06-10 15:14:47 -0700

    Initial commit

---
+++ BeachPulse/BeachPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachPulseApp.swift
+//  BeachPulse
+//
+//  Created by Matt Greenberg on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BeachPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeachPulseTests/BeachPulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeachPulseTests.swift
+//  BeachPulseTests
+//
+//  Created by Matt Greenberg on 6/10/25.
+//
+
+import Testing
+@testable import BeachPulse
+
+struct BeachPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
